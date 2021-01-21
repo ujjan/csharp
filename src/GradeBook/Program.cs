b9c9ab472693db668ce3de104b5d42d75c9f70a6
WICCON-GLAPTOP\Ali <ali@wiccon.se>
2021-01-21 14:48:12 +0100

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double result = 0;
+            List<double> grades = new List<double>() {12.4, 23.6, 13.5};
+
+                grades.Add(.5);
+
+            foreach (var number  in grades){
+                result += number;
+            }
+
+            result = result / grades.Count;
+
+            System.Console.WriteLine($"The average Grade is: {result:N2}");
+
+
+        }
+
+    }
+}
